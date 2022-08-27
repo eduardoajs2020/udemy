f0ca999dfb82151733b903c9f5d5564772f48bab
Eduardo dos Anjos Souza <eduardoajs2020@gmail.com>
2022-08-26 21:59:01 -0300

    g

---
+++ Blog/data/posts.php
@@ -0,0 +1,35 @@
+<?php
+
+$posts = [
+    [
+        'id'=> 1,
+        'title'=> 'Programando em PHP',
+        'description'=> 'Neste post você vai aprender tudo o que precisa para ser um bom programador em PHP',
+        'tags' => ['programação', 'PHP', 'lógica de programação'],
+        'img' => 'prog-1.jpg'
+    ],
+    [
+        'id'=> 2,
+        'title'=> 'Orientação a objetos com Java',
+        'description'=> 'Aprenda o paradigma de orientação a objetos com Java, uma linguagem incrível',
+        'tags' => ['Java', 'OOP', 'Programação avançada'],
+        'img' => 'prog-2.jpg'
+    ],
+    [
+        'id'=> 3,
+        'title'=> 'A importância da lógica de Programação',
+        'description'=> 'A lógica de programação é o ponto inicial de muitos estudantes de programação',
+        'tags' => ['lógica', 'programação', 'algoritmos'],
+        'img' => 'prog-3.jpg'
+    ],
+    [
+        'id'=> 4,
+        'title'=> 'Programação web',
+        'description'=> 'Neste post você vai aprender tudo o que precisa para programar na web',
+        'tags' => ['HTML', 'CSS', 'lógica de programação'],
+        'img' => 'prog-4.jpg'
+    ]
+];
+
+
+?>
---
+++ Blog/helpers/url.php
@@ -0,0 +1,4 @@
+<?php  
+//lINK QUE APONTA PARA ONDE ESTÁ A PASTA DE ESTILOS
+$BASE_URL = "http://". $_SERVER['SERVER_NAME'].':'.'3000' . dirname($_SERVER['REQUEST_URI'].'?'.'?');
+?>
