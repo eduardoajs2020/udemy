c94e01380ca6223aec07da39a4959382ab855524
Eduardo dos Anjos Souza <eduardoajs2020@gmail.com>
2022-10-13 22:39:22 -0300

    git push

--- Moviestar/editprofile.php
+++ Moviestar/editprofile.php
@@ -17,7 +17,7 @@ if($userData->image == ""){
 }
 
 ?>
-        <div id="main-container" class="container-fluid">
+        <div id="main-container" class="container-fluid edit-profile-page">
           <div class="col-md-12">
             <form action="<?=$BASE_URL?>user_process.php" method="POST" enctype="multipart/form-data">
                 <input type="hidden" name="type" value="update">
@@ -37,7 +37,7 @@ if($userData->image == ""){
                             <label for="email">E-mail:</label>
                             <input type="email" readonly class="form-control disabled" id="email" placeholder="Digite seu email" value="<?=$userData->email?>">
                         </div>
-                        <input type="submit" class="btn form-btn" value="Alterar">
+                        <input type="submit" class="btn card-btn" value="Alterar">
                     </div>
                     <div class="col-md-4">
                         <div id="profile-image-container" style="background-image: url('<?= $BASE_URL?>img/users/<?=$userData->image?>')"></div>
@@ -52,6 +52,24 @@ if($userData->image == ""){
                     </div>
                 </div>
             </form>
+            <div class="row" id="change-password-container">
+                <div class="col-md-4">
+                   <h2>Alterar a senha:</h2>
+                    <p class="page-description">Digite a nova senha e confirme, para alterar sua senha:</p>
+                    <form action="<?=$BASE_URL?>user_process.php" method="POST">
+                        <input type="hidden" name="type" value="changepassword" >
+                        <div class="form-group">
+                            <label for="password">Senha:</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Digite a sua nova senha">
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmpassword">Confirmação de senha:</label>
+                            <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirme a sua nova senha">
+                        </div>
+                        <input type="submit" class="btn card-btn" value="Alterar Senha">
+                    </form>
+                </div>
+            </div>
           </div>
         </div>
     <?php 
